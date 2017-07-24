c0f88fe32391411b6aaed7fe75e8df092d8f7ded
brownalex275 <brownalex275@gmail.com>
2017-07-23 20:05:16 -0400

    added comments

--- shutilDrill.py
+++ shutilDrill.py
@@ -1,13 +1,14 @@
+#Program written in Python 2.7 with the purpose of moving files from a desktop folder A to a desktop folder B
 import shutil
 import os
 
 folder = '/Users/nygfan72/Desktop/Folder A/'
 dest = '/Users/nygfan72/Desktop/Folder B'
 
-files = os.path.dirname(folder)
-folders = os.listdir(folder)
+files = os.path.dirname(folder) #get directory name of files in folder A
+folders = os.listdir(folder) #get list of items in folder A
 print(files)
-for item in folders:
+for item in folders: #move files from folder A to folder B and print the locations of the moved files
     print('file moved from ' + files+ '/' + str(item))
     source = files + '/' + str(item)
     shutil.move(source,dest)
